e862e0aaba74c25e0d4bb06c19bd3bf39b5906a2
Abdullah Bin Masaud <abdullah.mughal1080@gmail.com>
2026-09-14 04:09:31 +0500

    Purge dump_ah.py

--- dump_ah.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-
-for cv in ["CV__A.pdf", "CV__H.pdf"]:
-    print(f"\n==================== {cv} ====================")
-    with pdfplumber.open(rf"C:\Users\Lenovo\Desktop\CVV'S\{cv}") as pdf:
-        for page in pdf.pages:
-            print(page.extract_text())
